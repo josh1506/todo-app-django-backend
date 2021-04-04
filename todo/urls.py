aff28977d1a8f07c5862dd85d69afe5c6a633249
josh1506 <jmjabor@gmail.com>
2021-04-04 13:51:58 +0800

    add todo route

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import TodoView
+
+router = routers.DefaultRouter()
+router.register('list', TodoView)
 
 urlpatterns = [
-    # path('')
+    path('', include(router.urls))
 ]
