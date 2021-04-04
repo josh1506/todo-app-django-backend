029936d4cc255fffd2d3cf17e97ecff73ee61065
josh1506 <jmjabor@gmail.com>
2021-04-04 13:51:25 +0800

    add todo views

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializer import CheckListSerializer, TodoSerializer
+from .models import Todo, CheckList
+
+
+class TodoView(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
