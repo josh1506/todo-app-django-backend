4173e47d3b1a727bb3a3e076e698c471f1505d79
josh1506 <jmjabor@gmail.com>
2021-04-04 15:09:48 +0800

    add route for task

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import TodoView
+from .views import TodoView, CheckListView
 
 router = routers.DefaultRouter()
 router.register('list', TodoView)
+router.register('task', CheckListView)
 
 urlpatterns = [
     path('', include(router.urls))
