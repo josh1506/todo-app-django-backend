3c88bf684374be581adf0a1131b3c736dd1eba8a
josh1506 <jmjabor@gmail.com>
2021-04-04 13:20:07 +0800

    add todo and checklist model in admin dashboard

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Todo, CheckList
 
-# Register your models here.
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('user', 'memo', 'cover', 'status')
+    list_filter = ('user', 'memo', 'status')
+    search_fields = ('user', 'memo', 'status')
+    exclude = ('date_created',)
+
+
+@admin.register(CheckList)
+class CheckListAdmin(admin.ModelAdmin):
+    list_display = ('todo', 'text', 'status')
+    list_filter = ('todo', 'text', 'status')
+    search_fields = ('todo', 'text', 'status')
+    exclude = ('date_created',)
