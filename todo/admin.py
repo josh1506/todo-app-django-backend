440215212ee8d3c1427262405ad27ed653d83e5d
josh1506 <jmjabor@gmail.com>
2021-04-09 15:41:15 +0800

    Remove cover in admin dashboard

--- todo/admin.py
+++ todo/admin.py
@@ -4,15 +4,15 @@ from .models import Todo, CheckList
 
 @admin.register(Todo)
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ('user', 'memo', 'cover', 'status')
-    list_filter = ('user', 'memo', 'status')
+    list_display = ('id', 'user', 'memo', 'status')
+    list_filter = ('user', 'status')
     search_fields = ('user', 'memo', 'status')
     exclude = ('date_created',)
 
 
 @admin.register(CheckList)
 class CheckListAdmin(admin.ModelAdmin):
-    list_display = ('todo', 'text', 'status')
-    list_filter = ('todo', 'text', 'status')
+    list_display = ('id', 'todo', 'text', 'status')
+    list_filter = ('todo', 'status')
     search_fields = ('todo', 'text', 'status')
     exclude = ('date_created',)
