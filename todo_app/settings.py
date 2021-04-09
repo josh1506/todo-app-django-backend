3106c3544626303eaa644478241cc71bb1563dd7
josh1506 <jmjabor@gmail.com>
2021-04-09 17:26:50 +0800

    Update settings.py

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['todo-app-django-server.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['todo-app-django-server.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
